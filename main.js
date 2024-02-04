e89dbba81861169618c214ec2717ff02d0b15ac1
Le Van Anh <ank138795@gmail.com>
2024-02-04 18:57:45 +0700

    lan5

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const mongoose =  require('mongoose');
 const session =  require('express-session');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 //database connection
 mongoose.connect(
